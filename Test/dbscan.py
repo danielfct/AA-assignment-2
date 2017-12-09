b4289ee95ca360b8c81fad8eca28749e5dbf8e41
Daniel Pimenta <d.pimenta@campus.fct.unl.pt>
2017-12-09 13:15:33 +0000

    Moved all executed code of kmeans into tp2 main method

--- Test/dbscan.py
+++ Test/dbscan.py
@@ -1,27 +1,29 @@
 import numpy as np
-import sklearn
+from sklearn import neighbors
+from sklearn import cluster
 import matplotlib.pyplot as plt
 
 import cluster_analysis
 
-def k_distance(X, k= 4):
-    #First of all, I have to set the eps parameter of the classifier. To do so,
-    #I create a fictitious output vector, filled with ones and then applied the
-    #kNN classifier.
-    aux_label= np.zeros(X.shape[0])
-    neigh= sklearn.neighbors.KNeighborsClassifier(n_neighbors= 4)
-    neigh.fit(X, aux_label)
-    distances= neigh.kneighbors()
-    k_dist= np.zeros(X.shape[0])
+def k_distance(X, k=4):
+    """First of all, We have to set the eps parameter of the classifier. To do so,
+    we create a fictitious output vector, filled with zeros and then applied the
+    kNN classifier."""
+    
+    aux_label = np.zeros(X.shape[0])
+    knn = neighbors.KNeighborsClassifier(n_neighbors=4)
+    knn.fit(X, aux_label)
+    distances = knn.kneighbors()
+    k_dist = np.zeros(X.shape[0])
     for i in range(0, X.shape[0]):
-        k_dist[i]= distances[0][i,3]
+        k_dist[i] = distances[0][i,3]
     k_dist.sort()
-    k_dist= k_dist[::-1]
+    k_dist = k_dist[::-1]
     return k_dist
 
-def plot_k_distance(k_dist, k= 4):
-    fig= plt.figure()
-    plt.plot(range(0, k_dist.shape[0]), k_dist, label= str(k) + "-th distance")
+def plot_k_distance(k_dist, k=4):
+    fig = plt.figure()
+    plt.plot(range(0, k_dist.shape[0]), k_dist, label=str(k) + "-th distance")
     plt.xlabel("Points")
     plt.ylabel("Distance")
     plt.title(str(k) + "-th Distance")
@@ -32,50 +34,49 @@ def plot_k_distance(k_dist, k= 4):
 
 
 def dbscan_tuning(X, labels_true, min_eps, max_eps, pace):
-    '''This function computes the DBSCAN algorithm for the values comprised
+    """This function computes the DBSCAN algorithm for the values comprised
     between [eps-delta, eps+delta] if possible: if eps-delta is lesser than
     10 it sets 10, if eps+delta is greater than the biggest k-th distance it takes
-    the biggest k-th distance as maximum value.'''
-    values= np.arange(min_eps, max_eps, pace)
-    dbscan_indices= np.zeros((values.shape[0], 6))
-    dbscan_n_clusters= np.zeros(values.shape[0], dtype= np.int)
-    i= 0
+    the biggest k-th distance as maximum value."""
+    
+    values = np.arange(min_eps, max_eps, pace)
+    indices = np.zeros((values.shape[0], 6))
+    n_clusters = np.zeros(values.shape[0], dtype=np.int)
+    i = 0
     for curr_eps in np.arange(min_eps, max_eps, pace):
-        print(curr_eps)
-        dbscan= sklearn.cluster.DBSCAN(curr_eps, 4, n_jobs= -1)
-        dbscan.fit(X)
-        pred_labels = dbscan.labels_
-        #print(len(set(pred_labels)) - (1 if -1 in pred_labels else 0))
+        print("Current epsilon " + str(curr_eps))
+        dbscan_model = cluster.DBSCAN(curr_eps, 4, n_jobs= -1)
+        dbscan_model.fit(X)
+        pred_labels = dbscan_model.labels_
         n_clusters_ = len(set(pred_labels)) - (1 if -1 in pred_labels else 0)
+        n_clusters[i]= n_clusters_
         #print(n_clusters_)
-        dbscan_n_clusters[i]= n_clusters_
-        #print(dbscan_n_clusters[i])
-        dbscan_indices[i]= cluster_analysis.evaluate_cluster(X, labels_true, pred_labels)
+        indices[i]= cluster_analysis.evaluate_cluster(X, labels_true, pred_labels)
         i+= 1
-    return dbscan_indices, dbscan_n_clusters
+    return indices, n_clusters
         
 
-def dbscan_plot(dbscan_indices, min_eps, max_eps, pace):
-    index_name= ['Precision', 'Recall', 'F1Score', 'Rand Index', 'Adjusted Rand Index', 'Silhouette']
-    x_axis= np.arange(min_eps, max_eps, pace)
-    fig= plt.figure(figsize=(20,20))
+def plot_indices(indices, min_eps, max_eps, pace):
+    index_name = ['Precision', 'Recall', 'F1Score', 'Rand Index', 'Adjusted Rand Index', 'Silhouette']
+    x_axis = np.arange(min_eps, max_eps, pace)
+    fig = plt.figure(figsize=(20,20))
     for i in range(0,6):
         plt.subplot(3, 2, i+1)
         plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
         plt.title('DBSCAN ' + index_name[i])
-        plt.plot(x_axis, dbscan_indices[:,i])
+        plt.plot(x_axis, indices[:,i])
         plt.ylabel(index_name[i])
         plt.xlabel('Value of Epsilon')
     plt.show()
     fig.savefig('dbscan_indeces.pdf')
     plt.close()
 
-def dbscan_plot_cluster(dbscan_n_clusters, min_eps, max_eps, pace):
-    print(dbscan_n_clusters)
-    x_axis= np.arange(min_eps, max_eps, pace)
-    fig= plt.figure()
+def plot_cluster(n_clusters, min_eps, max_eps, pace):
+    #print(n_clusters)
+    x_axis = np.arange(min_eps, max_eps, pace)
+    fig = plt.figure()
     plt.title('DBSCAN - Number of clusters')
-    plt.plot(x_axis, dbscan_n_clusters.ravel())
+    plt.plot(x_axis, n_clusters.ravel())
     plt.ylabel('N. Cluster')
     plt.xlabel('Value of Epsilon')
     plt.show()
--- Test/tp2.py
+++ Test/tp2.py
@@ -1,10 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
 import sklearn
 from skimage.io import imread
-
 import data_processing
 import cluster_analysis
+import k_means
 import dbscan
     
 def plot_cartesian_coordinates(x, y, z):
@@ -51,21 +52,63 @@ def plot_classes(labels, longitude, latitude, alpha=0.5, edge='k'):
         plt.plot(x[mask], y[mask], '.', markersize=1, mew=1, markerfacecolor='w', markeredgecolor=edge)
     plt.axis('off') 
 
+
+def test_kmeans(X, labels_true, longitude, latitude):
+    
+    max_cluster= 150
+    kmeans_eval= k_means.kmeans_tuning(X, max_cluster, labels_true, 205)
+    k_means.plot_cluster(max_cluster, kmeans_eval)
+
+### TODO delete this and keep the loop on all index scores?
+    #Best cluster according to the Silhouette score
+    best_k= kmeans_eval[:,5].argmax() + 2
+    best_kmeans= sklearn.cluster.KMeans(25, random_state= 205).fit(X)
+    labels_pred = best_kmeans.labels_
+    plot_classes(labels_pred, longitude, latitude, alpha=0.5, edge='k')
+    best_kmean_eval= cluster_analysis.evaluate_cluster(X, labels_true, labels_pred)
+    print('\nMaximising Silhouette')
+    print('Number of clusters: %d' % best_k)
+    print("Precision: %0.3f" % best_kmean_eval[0])
+    print("Recall: %0.3f" % best_kmean_eval[1])
+    print("F1: %0.3f" % best_kmean_eval[2])
+    print("Rand Index: %0.3f" % best_kmean_eval[3])
+    print("Adjusted Rand Index: %0.3f" % best_kmean_eval[4])
+    print("Silhouette: %0.3f" % best_kmean_eval[5])
+
+
+    index_name= ['Precision', 'Recall', 'F1-Score', 'Rand Index', 'Adjusted Rand Index', 'Silhouette']
+    for i in range(0, 6):
+        best_k= kmeans_eval[:,i].argmax() + 2
+        best_kmeans_model = sklearn.cluster.KMeans(best_k, random_state= 205)
+        best_kmeans_model.fit(X)
+        labels_pred = best_kmeans_model.labels_
+        #plot_classes(labels_pred, longitude, latitude, alpha=0.5, edge='k')
+        best_kmeans_eval= cluster_analysis.evaluate_cluster(X, labels_true, labels_pred)
+        print('\nMaximising ' + index_name[i])
+        print('Number of clusters: %d' % best_k)
+        print("Precision: %0.3f" % best_kmeans_eval[0])
+        print("Recall: %0.3f" % best_kmeans_eval[1])
+        print("F1: %0.3f" % best_kmeans_eval[2])
+        print("Rand Index: %0.3f" % best_kmeans_eval[3])
+        print("Adjusted Rand Index: %0.3f" % best_kmeans_eval[4])
+        print("Silhouette: %0.3f" % best_kmeans_eval[5])
+
 def test_dbscan(X, labels_true, longitude, latitude, eps, delta, pace= 1):
-    k_dist= dbscan.k_distance(X)
+    #Calculate k_distances
+    k_dist = dbscan.k_distance(X)
     dbscan.plot_k_distance(k_dist)
     #We set epsilon to the distance we have at point 500
-    min_eps= max(10, eps-delta)
-    max_eps= min(k_dist.max(), eps+delta)
-    dbscan_indices, dbscan_n_clusters= dbscan.dbscan_tuning(X, labels_true, min_eps, max_eps, pace)
-    dbscan.dbscan_plot(dbscan_indices, min_eps, max_eps, pace)
-    dbscan.dbscan_plot_cluster(dbscan_n_clusters, min_eps, max_eps, pace)
-    eps_paper= k_dist[500]
+    min_eps = max(10, eps-delta)
+    max_eps = min(k_dist.max(), eps+delta)
+    indices, n_clusters = dbscan.dbscan_tuning(X, labels_true, min_eps, max_eps, pace)
+    dbscan.plot_indices(indices, min_eps, max_eps, pace)
+    dbscan.plot_cluster(n_clusters, min_eps, max_eps, pace)
+    eps_paper = k_dist[500]
     dbscan_model = sklearn.cluster.DBSCAN(eps_paper, 4, n_jobs=-1)
     dbscan_model.fit(X)
-    pred_labels = dbscan.labels_
+    pred_labels = dbscan_model.labels_
     n_clusters_ = len(set(pred_labels)) - (1 if -1 in pred_labels else 0)
-    dbscan_evaluate_paper= cluster_analysis.evaluate_cluster(X, labels_true, pred_labels)
+    dbscan_evaluate_paper = cluster_analysis.evaluate_cluster(X, labels_true, pred_labels)
     print('Number of clusters: %d' % n_clusters_)
     print("Precision: %0.3f" % dbscan_evaluate_paper[0])
     print("Recall: %0.3f" % dbscan_evaluate_paper[1])
@@ -79,11 +122,14 @@ def main():
     # Get the data
     latitude, longitude, fault = data_processing.read_csv();
     x, y, z = data_processing.transform_coordinates(latitude, longitude);
-    #plot_cartesian_coordinates(x, y, z);
+    plot_cartesian_coordinates(x, y, z);
     X= data_processing.preprocess_data(x, y, z)
     
+    #KMEANS
+    test_kmeans(X, fault, longitude, latitude)
+    
     # DBSCAN
-    test_dbscan(X, fault, longitude, latitude, 300, 300, 1)
+    test_dbscan(X, fault, longitude, latitude, 300, 300)
     #TODO: compute the indices excluding the noise!
     
      ## TODO do the same for kmeans and gaussian
